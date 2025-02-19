20840aff6f2d4ab2d2271f70e444b0a9ffdcd1d1
innocurve <admin@inno-curve.com>
2025-02-19 15:37:38 +0900

    진짜진짜진짜 수정

--- app/greeting/page.tsx
+++ app/greeting/page.tsx
@@ -92,8 +92,10 @@ export default function GreetingVideo() {
             <svg className="absolute top-0 left-0 w-12 sm:w-16 h-12 sm:h-16 text-gray-400 transform -translate-x-1/3 -translate-y-1/3 sm:-translate-x-1/4 sm:-translate-y-1/4 z-0" viewBox="0 0 24 24" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
               <path d="M14.017 21v-7.391c0-5.704 3.731-9.57 8.983-10.609l.995 2.151c-2.432.917-3.995 3.638-3.995 5.849h4v10h-9.983zm-14.017 0v-7.391c0-5.704 3.748-9.57 9-10.609l.996 2.151c-2.433.917-3.996 3.638-3.996 5.849h3.983v10h-9.983z"/>
             </svg>
-            <h1 className="text-lg sm:text-xl md:text-3xl font-bold mb-4 sm:mb-6 text-center whitespace-pre-line mx-12 sm:mx-8">
-              {translate('greetingTitle', language)}
+            <h1 className="text-lg sm:text-xl md:text-3xl font-bold mb-4 sm:mb-6 text-center mx-12 sm:mx-8">
+              {translate('greetingTitle', language).split('\n').map((line, i) => (
+                <span key={i} className="block">{line}</span>
+              ))}
             </h1>
             <p className="text-lg relative z-10">
               {translate('greetingDescription', language).split('\n').map((line, i) => (
