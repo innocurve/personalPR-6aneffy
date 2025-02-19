57faa5548d44d3f8f9eac44dc3d21cbab5767069
innocurve <admin@inno-curve.com>
2025-02-19 15:27:08 +0900

    진짜 수정

--- app/greeting/page.tsx
+++ app/greeting/page.tsx
@@ -92,11 +92,9 @@ export default function GreetingVideo() {
             <svg className="absolute top-0 left-0 w-16 h-16 text-gray-400 transform -translate-x-1/4 -translate-y-1/4 z-0" viewBox="0 0 24 24" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
               <path d="M14.017 21v-7.391c0-5.704 3.731-9.57 8.983-10.609l.995 2.151c-2.432.917-3.995 3.638-3.995 5.849h4v10h-9.983zm-14.017 0v-7.391c0-5.704 3.748-9.57 9-10.609l.996 2.151c-2.433.917-3.996 3.638-3.996 5.849h3.983v10h-9.983z"/>
             </svg>
-            <div className="relative z-10 text-2xl font-bold mb-4">
-              {translate('greetingTitle', language).split('\n').map((line, i) => (
-                <p key={i}>{line}</p>
-              ))}
-            </div>
+            <h1 className="text-lg sm:text-xl md:text-3xl font-bold mb-4 sm:mb-6 text-center whitespace-pre-line mx-8">
+              {translate('greetingTitle', language)}
+            </h1>
             <p className="text-lg relative z-10">
               {translate('greetingDescription', language).split('\n').map((line, i) => (
                 <span key={i}>{line}<br /></span>
